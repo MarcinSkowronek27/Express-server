06d777651ebefee1e13c2b8308141a216415976d
Marcin Skowronek <marcinfoto35@op.pl>
2021-10-08 10:10:07 +0200

    Add handlebars and dark option

--- server.js
+++ server.js
@@ -4,39 +4,33 @@ const hbs = require('express-handlebars');
 
 
 const app = express();
-
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
-app.use((req, res, next) => {
-  res.show = (name) => {
-    res.sendFile(path.join(__dirname, `/views/${name}`));
-  };
-  next();
+
+app.get('/hello/:name', (req, res) => {
+  res.render('hello', { name: req.params.name });
 });
 
 app.get('/', (req, res) => {
-  res.show('index.html');
+  res.render('index')
 });
 
 app.get('/about', (req, res) => {
-  res.show('about.html');
+  res.render('about.hbs', { layout: 'dark' });
 });
 
 app.get('/contact', (req, res) => {
-  res.show('contact.html');
+  res.render('contact')
 });
 
 app.get('/info', (req, res) => {
-  res.show('info.html');
+  res.render('info')
 });
 
 app.get('/history', (req, res) => {
-  res.show('history.html');
+  res.render('history')
 });
 
-app.get('/hello/:name', (req, res) => {
-  res.render('hello', { layout: false, name: req.params.name });
-});
 
 app.use(express.static(path.join(__dirname, '/public')));
 
